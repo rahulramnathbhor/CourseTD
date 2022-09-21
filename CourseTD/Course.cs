18c02b831ec3dc808b6c426f9706305ae1ef6e95
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-21 16:21:09 +0530

    Add project files.

---
+++ CourseTD/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseTD
+{
+
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Fees { get; set; }
+
+        public override string ToString()
+        {
+            return $"Course Details : {Id}  {Name}  {Fees}";
+        }
+    }
+
+}
---
+++ CourseTD/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseTD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course[] course=        
+                {
+                new Course{Id=1,Name="BCS",Fees=26000},
+                new Course{Id=2,Name="BCA",Fees=19000},
+                new Course{Id=3,Name="BBA",Fees=25050},
+                new Course{Id=4,Name="MCA",Fees=55000},
+                new Course {Id=5,Name="Think Quotient", Fees=153400}
+
+            };
+            foreach (Course  c in course)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+    }
+}
